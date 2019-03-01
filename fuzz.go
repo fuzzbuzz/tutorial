3edbad445b09a12f5eb13c111fe0dd34ac211ed2
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-03-01 09:04:58 +0300

    Make Go example slightly more idiomatic.

--- fuzz.go
+++ fuzz.go
@@ -2,9 +2,9 @@ package tutorial
 
 // FuzzerEntrypoint is the method Fuzzbuzz will repeatedly
 // run with new tests to try and find bugs in BrokenMethod
-func FuzzerEntrypoint(Data []byte) int {
-	// Convert Data to the type we need:
-	testString := string(Data)
+func FuzzerEntrypoint(data []byte) int {
+	// Convert data to the type we need:
+	testString := string(data)
 	BrokenMethod(testString)
 	return 0
 }
--- method.go
+++ method.go
@@ -1,11 +1,11 @@
 package tutorial
 
 // BrokenMethod has a bug - it will try to read the 4th
-// index of Data even when it only has a length of 3.
-func BrokenMethod(Data string) bool {
-	return len(Data) >= 3 &&
-		Data[0] == 'F' &&
-		Data[1] == 'U' &&
-		Data[2] == 'Z' &&
-		Data[3] == 'Z'
+// index of data even when it only has a length of 3.
+func BrokenMethod(data string) bool {
+	return len(data) >= 3 &&
+		data[0] == 'F' &&
+		data[1] == 'U' &&
+		data[2] == 'Z' &&
+		data[3] == 'Z'
 }
